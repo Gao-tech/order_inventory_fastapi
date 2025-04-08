0c43a91b4480743806c5395db86caf0445479812
Jessi <jessi_gao@163.com>
2025-04-08 16:20:41 +0200

    restructured code for AWS SAM deployment

---
+++ src/app/routers/order.py
@@ -0,0 +1,112 @@
+import datetime
+from fastapi import FastAPI, APIRouter, Path, Query, Response, status, HTTPException, Depends
+# from sqlalchemy import delete, func
+from ..models import DeliveryMethod, Order, OrderCreate, OrderItem, OrderOut, Stock, User, Product
+from sqlmodel import Session, select
+from typing import Annotated, Optional
+from ..db import get_session
+from ..oauth2 import get_current_user
+from sqlalchemy.orm import selectinload
+
+router = APIRouter(tags=['Order']) 
+
+#centralized delivery from the same region and loggin user to make order is optional
+def calculate_arrival_date(created_at: datetime.datetime) -> datetime:
+    return created_at + datetime.timedelta(days=3)
+
+@router.post("/orders", response_model=OrderOut)
+def create_order(order_data: OrderCreate,
+                 current_user: Optional[User] = Depends(get_current_user),
+                 session: Session = Depends(get_session)) -> Order:
+    
+
+
+    # Calculate total product price
+    total = 0.0
+    items_to_save = []
+    
+    for item in order_data.items:
+        # Get product price
+        product = session.get(Product, item.product_id)
+        if not product:
+            raise HTTPException(404, f"Product {item.product_id} not found")
+    
+    # Search item in stock
+        stock = session.exec(
+            select(Stock).where(
+                (Stock.product_id == item.product_id) & 
+                (Stock.warehouse_id == order_data.warehouse_id))).first()
+    
+        if not stock or stock.quantity < item.quantity:
+            raise HTTPException(
+                status_code=400,
+                detail=f"Not enough stock for product {item.product_id} in warehouse {order_data.warehouse_id}")
+        
+        total += product.price * item.quantity
+        items_to_save.append(item)
+
+    def calculate_cost(method: DeliveryMethod) -> int:
+        match method:
+            case DeliveryMethod.SELF_PICKUP:
+                return 50
+            case DeliveryMethod.CURBSIDE:
+                return 400
+            case DeliveryMethod.INDOOR_DELIVERY:
+                return 800
+
+    # Generate timestamps
+    created_at = datetime.datetime.now()  # Local naive datetime
+    arrival_date = calculate_arrival_date(created_at)  # Pass created_at explicitly
+
+    # Create order with timestamps
+    new_order = Order(
+        user_zipcode=order_data.user_zipcode,
+        delivery_method=order_data.delivery_method,
+        cost=calculate_cost(order_data.delivery_method),
+        total_price=total,
+        created_at=created_at,  # Add this field
+        arrival_date=arrival_date,
+        warehouse_id=order_data.warehouse_id,
+        user_id=current_user.id if current_user else None  #make guest user and login user both can make order
+    )
+    session.add(new_order)
+    session.flush()  # Ensure the new_order.uuid is generated and persisted in DB
+
+    # Create order items
+    for item in order_data.items:
+        order_item = OrderItem(
+            order_id=new_order.uuid,  
+            product_id=item.product_id,
+            quantity=item.quantity,
+            price_at_order=product.price
+        )
+        session.add(order_item)    
+
+    # Reduce stock quantities
+    for item in order_data.items:
+        stock = session.exec(
+            select(Stock).where(
+                (Stock.product_id == item.product_id) & 
+                (Stock.warehouse_id == order_data.warehouse_id)
+            )
+        ).first()
+        if stock:
+            stock.quantity -= item.quantity
+            session.add(stock)
+
+    session.commit() 
+    return new_order
+
+
+#login user get all its own orders
+@router.get("/orders", response_model=list[OrderOut])
+def get_orders(
+    session: Session = Depends(get_session),
+    current_user: Optional[User] = Depends(get_current_user)  # Optional dependency
+):
+    orders = session.exec(select(Order).where(Order.user_id == current_user.id)).all()
+    
+    if not orders:
+        raise HTTPException(status_code=404, detail="No orders found")
+    
+    return orders
---
+++ src/app/routers/product.py
@@ -0,0 +1,91 @@
+from fastapi import FastAPI, APIRouter, Path, Query, Response, status, HTTPException, Depends
+from sqlalchemy import delete, func
+from ..models import Product, StockOut, User, Stock, ProductWithStockOut
+from sqlmodel import Session, select
+from typing import Annotated
+from ..db import get_session
+from ..oauth2 import get_admin
+from sqlalchemy.orm import selectinload
+
+
+router = APIRouter(tags=['Products']) 
+
+
+@router.get('/products', response_model=list[Product])
+async def get_products(session: Session=Depends(get_session)) ->list[Product]:
+    products = session.exec(select(Product)).all()
+    return products
+
+
+@router.get('/products/{product_id}',response_model=ProductWithStockOut)
+async def get_product_id(product_id: Annotated[str, Path(title="IKEA article number")],
+                         session: Session=Depends(get_session)) -> ProductWithStockOut:
+ 
+    product = session.exec(select(Product).where(Product.id==product_id)
+                           .options(selectinload(Product.stock).selectinload(Stock.warehouse))).first()
+    if product is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"The product with id {product_id} is not found")
+
+    return ProductWithStockOut(
+        id=product.id,
+        name=product.name,
+        category=product.category,
+        price=product.price,
+        series=product.series,
+        stock=[StockOut(
+            warehouse_id=stock.warehouse_id,
+            quantity=stock.quantity
+        )
+        for stock in product.stock
+        ]
+    )
+
+  
+#only loggin admin can post product:  haven't added
+@router.post('/products', status_code=status.HTTP_201_CREATED, response_model=ProductWithStockOut)
+async def create_product(
+    product_data: ProductWithStockOut,
+    session: Session = Depends(get_session)
+) -> ProductWithStockOut:
+    
+    # Check for existing product
+    existing_product = session.exec(select(Product).where(Product.id == product_data.id)).first()
+    if existing_product:
+        raise HTTPException(status_code=400, detail="Product already exists, please update it")
+
+    # Create Product
+    new_product = Product(
+        id=product_data.id,
+        name=product_data.name,
+        category=product_data.category,
+        price=product_data.price,
+        series=product_data.series
+    )
+    session.add(new_product)
+    
+    # Create Stock entries
+    for stock_item in product_data.stock:
+        stock = Stock(
+            product_id=product_data.id,
+            warehouse_id=stock_item.warehouse_id,
+            quantity=stock_item.quantity
+        )
+        session.add(stock)
+    
+    session.commit()
+    session.refresh(new_product)
+    
+    return new_product
+
+
+#only loggin admin can update product:  haven't done
+@router.put('products/{product_id}')
+async def update_product_stock():
+    pass
+
+#only loggin admin can delete product:  haven't done
+@router.delete('product/{product_id}')
+async def delete_product():
+    pass
+
+
